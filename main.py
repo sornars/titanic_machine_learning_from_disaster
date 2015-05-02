6d16930fbbe47eaf743224be67550cb1cf202001
Shivan Sornarajah <kaggle@mail.sornars.com>
2015-05-02 12:20:18 +0100

    Added cross validation.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 from sklearn.naive_bayes import GaussianNB
+from sklearn.metrics import classification_report
 
 def munge_data(csv_input):
     """Take train and test set and make them useable for machine learning algorithms."""
@@ -31,27 +32,39 @@ def munge_data(csv_input):
 def main():
     train_df = munge_data('./data/train.csv')
     train_df = train_df.drop('PassengerId', axis=1)
+
+    cx_df = train_df[800:]
+    cx_target_df = cx_df['Survived']
+    cx_df = cx_df.drop('Survived', axis=1)
+    cx_df = cx_df.sort(axis=1)
+    
+    train_df = train_df[:800]
     target_df = train_df['Survived']
     train_df = train_df.drop('Survived', axis=1)
     train_df = train_df.sort(axis=1)
-
 
     test_df = munge_data('./data/test.csv')
     test_ids = test_df.PassengerId.values
     test_df = test_df.drop('PassengerId', axis=1)
     test_df = test_df.sort(axis=1)
     
+    cx_data = cx_df.values
+    cx_target_data = cx_target_df.values
     train_data = train_df.values
     target_data = target_df.values
     test_data = test_df.values
 
     gnb = GaussianNB()
     gnb.fit(train_data, target_data)
-    output = gnb.predict(test_data)
+    cx_predictions = gnb.predict(cx_data)
+
+    print(classification_report(cx_target_data, cx_predictions))
+
+    predictions = gnb.predict(test_data)
 
     with open('output.csv', 'w') as o:
         o.write('PassengerId,Survived\n')
-        for passenger, prediction in zip(test_ids, output):
+        for passenger, prediction in zip(test_ids, predictions):
             o.write('{},{}\n'.format(passenger, prediction))
 
 if __name__ == '__main__':
