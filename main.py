dd3d8d5c34aef273fcf4e1668d56939ede08e509
Shivan Sornarajah <kaggle@mail.sornars.com>
2015-05-04 20:34:02 +0100

    Added recursive feature selection and Cabin feature.

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 import numpy as np
 from sklearn import cross_validation, svm
+from sklearn.feature_selection import RFECV
 from sklearn.metrics import classification_report
+
 
 gender_map =  {'female': 0, 'male': 1}
 embarked_map = {'C': 0, 'S': 1, 'Q': 2}
@@ -12,12 +15,12 @@ title_map = {'capt.': 0, 'col.': 1, 'countess.': 2, 'don.': 3, 'dr.': 4,
 pclasses = [1, 2, 3]
 ticket_map = {'0': 3, 'A': 0, 'C': 4, 'F': 6, 'L': 7, 'P': 1, 'S': 2, 'W': 5}
 cabin_map = {'A': 5, 'B': 6, 'C': 1, 'D': 4, 'E': 2, 'F': 7, 'G': 3, 'T': 8, 'U': 0}
-median_age = None
+median_age_by_class = pd.Series()
 median_fare_by_class = pd.Series()
 
 def munge_data(csv_input):
     """Take train and test set and make them useable for machine learning algorithms."""
-    global median_age, median_fare_by_class
+    global median_age_by_class, median_fare_by_class
     df = pd.read_csv(csv_input)
     
     df['Sex'] = df['Sex'].map(gender_map)
@@ -26,21 +29,23 @@ def munge_data(csv_input):
 
     df['Title'] = df['Name'].str.lower().str.extract('([a-z]+\.)').map(title_map)
 
-    if not median_age:
-        median_age = df['Age'].dropna().median()
-    df['Age'] = df['Age'].fillna(median_age)
+    if median_age_by_class.empty:
+        median_age_by_class = df.groupby(['Pclass'])['Age'].median()
 
     if median_fare_by_class.empty:
         median_fare_by_class = df.groupby(['Pclass'])['Fare'].median()
+    
     for pclass in pclasses:
+        df.loc[(df['Age'].isnull()) & (df['Pclass'] == pclass), 'Age'] = median_age_by_class[pclass]
         df.loc[(df['Fare'].isnull()) & (df['Pclass'] == pclass), 'Fare'] = median_fare_by_class[pclass]
 
     df['Child'] = (df['Age'] < 18).astype(int)
     df['Age*Class'] = df['Age'] * df['Pclass']
     df['FamilySize'] = df['SibSp'] + df['Parch']
+    df['HasFamily'] = (df['FamilySize'] > 0).astype(int)
     df['Ticket'] = df['Ticket'].str.split(' ').str[0].replace('^[0-9]+$', '0', regex=True).str[0].map(ticket_map)
-    
-    df = df.drop(['Name', 'Cabin'], axis=1)
+    df['Cabin'] = df['Cabin'].str[0].fillna('U').map(cabin_map)
+    df = df.drop(['Name'], axis=1)
     
     return df
 
@@ -60,21 +65,18 @@ def main():
     target_data = target_df.values
     test_data = test_df.values
 
-    scores = []
-    for i in range(0, 5):
-        train_data, cx_data, target_data, cx_target_data = cross_validation.train_test_split(
-            train_data, target_data, test_size=0.2)
+    clf = svm.SVC(kernel='linear')
+    selector = RFECV(clf, step=1, cv=5, scoring='accuracy')
+    
+    train_data, cx_data, target_data, cx_target_data = cross_validation.train_test_split(
+        train_data, target_data, test_size=0.2)
 
-        clf = svm.SVC(kernel='linear')
-        clf.fit(train_data, target_data)
-        scores.append(clf.score(cx_data, cx_target_data))
-        cx_predictions = clf.predict(cx_data)
-        print(classification_report(cx_target_data, cx_predictions))
-
-    avg_score = sum(scores)/len(scores)
-    print(avg_score)
-
-    predictions = clf.predict(test_data)
+    selector = selector.fit(train_data, target_data)
+    
+    print(selector.score(cx_data, cx_target_data))
+    cx_predictions = selector.predict(cx_data)
+    print(classification_report(cx_target_data, cx_predictions))
+    predictions = selector.predict(test_data)
 
     with open('output.csv', 'w') as o:
         o.write('PassengerId,Survived\n')
